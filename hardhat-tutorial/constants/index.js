910fa0f04ca1827edd28846d5789a1f4f2c12735
vidhanarya <vidhanarya50@gmail.com>
2022-10-30 19:25:55 +0530

    Create CryptoDevs DAO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xB307218AeDCE45c697Af1DCa5B0D6C7d4a63e237";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
